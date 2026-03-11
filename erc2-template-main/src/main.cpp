e199036a9003ae4f9ac07801980fe4940ece4cc9
marottiluc <marotti.13@buckeyemail.osu.edu>
2026-03-11 14:25:04 -0400

    cds testing

--- erc2-template-main/src/main.cpp
+++ erc2-template-main/src/main.cpp
@@ -7,12 +7,16 @@
 // FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
 // FEHServo servo(FEHServo::Servo0);
 
+AnalogInputPin CdS_Cell(FEHIO::Pin0);
 
 void ERCMain()
 {
-    // Your code here!
+    while(true)
+    {
+        float value = CdS_Cell.Value();
+        LCD.Write(value);
+        LCD.Clear();
 
-    // Or just use the TestGUI function
-    TestGUI();
+    }
 
 }
